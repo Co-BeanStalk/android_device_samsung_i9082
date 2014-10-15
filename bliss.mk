85f73e2368d4ef1d61a6b0b52dcb50b8d6b54f1f
Jack <jackeagle102@gmail.com>
2014-10-15 15:04:45 +0530

    Change To Bliss

--- bliss.mk
+++ bliss.mk
@@ -1,18 +1,18 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/beanstalk/config/gsm.mk)
+$(call inherit-product, vendor/bliss/config/gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := GalaxyGrandDuos
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/beanstalk/config/common_full_phone.mk)
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/i9082/device_i9082.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := i9082
-PRODUCT_NAME := beanstalk_i9082
+PRODUCT_NAME := bliss_i9082
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := GT-I9082
